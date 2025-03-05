7c9f1ddd05ab35388d03b15bbc163ebe0dc924fe
Justin Torres <justin.torres0007@gmail.com>
2025-03-04 20:57:30 -0600

    small fix

--- app/@modal/[modal]/page.tsx
+++ app/@modal/[modal]/page.tsx
@@ -3,9 +3,14 @@ import LoginModal from "@/modals/Login";
 import ScheduledEvent from "@/modals/ScheduledEvent";
 import SettingsModal from "@/modals/Settings";
 import { JSX } from "react";
-import { PageProps } from "../../../../.next/types/app/layout";
 
-const ModalPage = async({ params }: PageProps) => {
+type PageProps = {
+  params: {
+    modal: string;
+  }
+}
+
+const ModalPage = async({ params }: PageProps ) => {
   const { modal } = await params;
   console.log(modal)
   const modalContent: { [key: string]: JSX.Element } = {
